e4f75e1d0eac33bc62c05fe6afe150463d898aae
Geymark-sw <emmanuelpapio12@gmail.com>
2025-09-04 11:04:43 +0200

    creazione dei model

--- src/main/java/model/Prodotto.java
+++ src/main/java/model/Prodotto.java
@@ -1,5 +1,32 @@
 package model;
 
-public class Prodotto {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import util.Tipo;
 
+@Entity
+@Table(name = "prodotto")
+public class Prodotto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_prodotto")
+	private long id_prodotto;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "descrizione")
+	private String descrizione;
+	
+	@Column(name = "prezzo")
+	private float prezzo;
+	
+	@Column(name = "tipo")
+	private Tipo tipo;
+	
 }
